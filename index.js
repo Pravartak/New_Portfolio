d13684e7a9c5d49a9ca00b3c9d9149c4f1a27ed8
Pravartak <pravartakambhore@gmail.com>
2026-09-10 18:43:37 +0530

    Two new tabs for skills and projects.

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
-const skillsInput = document.getElementById("arrow1");
-const projectsInput = document.getElementById("arrow2");
-const iconDown1 = document.getElementById("iconDown1");
-const iconDown2 = document.getElementById("iconDown2");
-const skills = document.querySelector("skills");
-const projects = document.querySelector("projects");
+let skillsInput = document.getElementById("arrow1");
+let projectsInput = document.getElementById("arrow2");
+
+let iconDown1 = document.getElementById("iconDown1");
+let iconDown2 = document.getElementById("iconDown2");
+
+let skills = document.querySelector("skills");
+let projects = document.querySelector("projects");
+
 console.log(skillsInput);
+
+// if (skillsInput.checked) {
+//     iconDown1.style.transform = "rotate(180deg)";
+// } else {
+//     iconDown1.style.transform = "rotate(0deg)";
+// }
+
 skillsInput.addEventListener("change", () => {
-    if (this.checked) {
-        iconDown1.src = "assets/chevron-up.svg";
+    if (skillsInput.checked) {
+        iconDown1.style.rotate = "180deg";
     } else {
-        iconDown1.src = "assets/chevron-down.svg";
+        iconDown1.style.rotate = "0deg";
+    }
+});
+
+projectsInput.addEventListener("change", () => {
+    if (this.checked) {
+        iconDown2.style.transform = "rotate(180deg)";
+    } else {
+        iconDown2.style.transform = "rotate(0deg)";
     }
 });
